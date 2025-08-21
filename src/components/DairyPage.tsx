b032830f2c1b8b677478fc0d0890a17c79a684bd
Karan Bhardwaj <your-github-karan685bhardwaj@gmail.com.com>
2025-08-21 13:35:22 +0530

    fix: resolved merge conflict in DairyPage.tsx

--- src/components/DairyPage.tsx
+++ src/components/DairyPage.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import CategoryPage from './CategoryPage';
 
@@ -15,21 +14,3 @@ const DairyPage: React.FC = () => {
 };
 
 export default DairyPage;
-=======
-import React from 'react';
-import CategoryPage from './CategoryPage';
-
-const DairyPage: React.FC = () => {
-  return (
-    <CategoryPage
-      category="Dairy"
-      categoryTitle="Dairy Delights"
-      categoryDescription="Fresh, rich, and creamy dairy essentials: milk, paneer, ghee, and more."
-      categoryImage="/images/DairyProducts.jpg"
-      itemLabelPlural="products"
-    />
-  );
-};
-
-export default DairyPage;
->>>>>>> 0f2845e6b5b2b482e8c9f5bbce87ea359dbc2bbc
